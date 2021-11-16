4492abc7024ae49d46cf90084597e23607fed81a
Ferid HELALI <ferid.helali@upgrade-code.org>
2021-11-16 15:38:46 +0100

    Car exemple

---
+++ src/oriented_object_sample.js
@@ -0,0 +1,67 @@
+// 4 Objects
+
+class Car{
+    engine=null;
+    constructor(brand,matricule,engine){
+      this.brand=brand;
+      this.matricule=matricule; 
+      this.engine=engine; 
+    }
+
+    get Brand(){
+        return this.brand;
+    }
+
+    set Brand (newValue){
+        this.brand=newValue;
+    }
+
+    start(){
+        this.engine.start();
+    }
+
+    accelerate(dv){
+       console.log('Accelerate the speed of the car');
+       this.engine.accelerate(10);
+       console.log(this.engine.speed);
+    };
+
+    deccelerate(dv){
+        console.log('Deccelerate the speed of the car');
+       this.engine.deccelerate(10);
+       console.log(this.engine.speed);
+    }
+}
+
+class Engine{
+    speed=0;
+    constructor(speed){
+        this.speed=speed;
+    }
+    start(){
+        console.log("the engine is starting now ...")    
+    }
+    accelerate(dv){
+        this.speed+=dv;
+        console.log(`the engine is accelerating now ... @ ${this.speed}`)  
+      
+    };
+
+    deccelerate(dv){
+        this.speed-=dv;
+        console.log(`the engine is deccelerating now ... @ ${this.speed}`)  
+    }
+}
+
+class Wheel {
+
+}
+
+class Application{
+    run(){
+        console.log('main program')
+    }
+}
+
+app=new Application();
+app.run();
